2e9eb119233892071fada3b241d689e9596575a8
Yann@SeenKid <30565751+SeenKid@users.noreply.github.com>
2023-08-30 09:31:29 +0200

    Create RemoveWCDashboard.php

---
+++ RemoveWCDashboard/RemoveWCDashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: RemoveWCDashboard
+ * Author: SeenKid
+ * Version: 1.2
+ * Description: Remove woocommerce dashbnoard without removing login
+ * Plugin URI: https://github.com/SeenKid/RemoveWCDashboard
+ */
+
+ function WOO_account_menu_items($items) {
+    unset($items['dashboard']);
+    return $items;            
+}
+
+add_filter ('woocommerce_account_menu_items', 'WOO_account_menu_items');
+
+?>
